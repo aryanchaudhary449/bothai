d7996cee2f723e84c26d28feff6c27258108715c
aryanchy4499 <158821779+aryanchy4499@users.noreply.github.com>
2024-07-13 12:06:57 +0530

    Update shorteners.py

--- bot/helper/ext_utils/shorteners.py
+++ bot/helper/ext_utils/shorteners.py
@@ -2,7 +2,8 @@ from base64 import b64encode
 from random import choice, random, randrange
 from time import sleep
 from urllib.parse import quote
-
+import requests 
+import urllib
 from cloudscraper import create_scraper
 from urllib3 import disable_warnings
 
